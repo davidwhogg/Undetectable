6924fd14c50d246aeafd8f330a2ce7d201820931
David W. Hogg <dwhogg@gmail.com>
2013-02-12 10:18:49 -0500

    make fake data, doesn't write anything out yet

--- code/ersatz.py
+++ code/ersatz.py
@@ -7,21 +7,50 @@ purpose: Make ersatz exoplanet data.
 
 import numpy as np
 
+
 def make_blank(N, M, timelim=[0., 1000.], sigmalim=[1., 4.]):
+    """
+    Choose times and uncertainties and make radial velocity curves
+    with zero mean and heteroskedastic Gaussian noise with known
+    variances.
+    """
     result = []
     for nn in range(N):
-        times = timelim[0] + (timelim[1] - timelim[0]) * np.random.uniform(size=M).sort()
+        times = timelim[0] + (timelim[1] - timelim[0]) * np.sort(np.random.uniform(size=M))
         sigmas = sigmalim[0] + (sigmalim[1] - sigmalim[0]) * np.random.uniform(size=M)
         rvs = np.zeros_like(times)
         rvs += sigmas * np.random.normal(size=M)
-        result += (times, sigmas, rvs)
+        result.append((times, sigmas, rvs))
     return result
 
-def add_stack(blanks):
-    return 1. * blanks
+def add_stack(blanks, amp=0.25, period=187., phase=0.113):
+    """
+    Add the exact same exoplanet into every radial velocity curve.
+    """
+    result = []
+    for blank in blanks:
+        times, sigmas, brvs = blank
+        rvs = 1. * brvs + amp * np.cos(2. * np.pi / period + phase)
+        result.append((times, sigmas, rvs))
+    return result
 
-def add_ersatzs(blanks):
-    return 1. * blanks
+def ersatz_prior_draw():
+    """
+    Draw an exoplanet from a crazy, hard-set prior.
+    """
+    phase = 2. * np.pi * np.random.uniform()
+    period = 180 + 40 * np.random.normal()
+    amp = 0.25 + 0.05 * np.random.normal() 
+    return amp, period, phase
+
+def add_ersatz(blanks):
+    result = []
+    for blank in blanks:
+        times, sigmas, brvs = blank
+        amp, period, phase = ersatz_prior_draw()
+        rvs = 1. * brvs + amp * np.cos(2. * np.pi / period + phase)
+        result.append((times, sigmas, rvs))
+    return result
 
 def make_all(N, M):
     blanks = make_blank(N, M)
@@ -30,4 +59,5 @@ def make_all(N, M):
     return blanks, stacks, ersatzs
 
 if __name__ == "__main__":
-    bs, ss, es = make_all(10, 100)
+    np.random.seed(42)
+    bs, ss, es = make_all(1000, 100)
