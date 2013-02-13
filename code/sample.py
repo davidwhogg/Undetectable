373a00d8658dffa709fddb90ed44d8158af21085
David W. Hogg <dwhogg@gmail.com>
2013-02-13 13:53:29 -0500

    containers

---
+++ code/sample.py
@@ -0,0 +1,12 @@
+"""
+This file is part of the Undetectable project.
+Copyright 2013 David W. Hogg.
+
+purpose
+-------
+* Sample with default priors in prep for hierarchical importance sampling.
+
+bugs
+----
+* Not yet written.
+"""
---
+++ code/stack.py
@@ -0,0 +1,12 @@
+"""
+This file is part of the Undetectable project.
+Copyright 2013 David W. Hogg.
+
+purpose
+-------
+* Stack data to reveal stationary signals.
+
+bugs
+----
+* Not yet written.
+"""
